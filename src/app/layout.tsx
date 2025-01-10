95d734c8dd8ed77c0483918cbab912673014792b
Thiago Furuta <thiago.furuta@gmail.com>
2025-01-10 11:21:02 -0300

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "./components/header";
+
+export const metadata: Metadata = {
+  title: "Controle Financeiro",
+  description: "Sistema para a gestão financeira pessoal",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Header></Header>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const [user, setUser] = useState({});
+
+export default function Perfil() {
+  return (
+    <h1>perfil</h1>
+  )
+}
